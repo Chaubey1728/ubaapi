4d7d456b721b56ecb93387777160186d34c34b17
Chaubey1728 <4017amitchaubey@gmail.com>
2022-12-14 23:20:52 +0530

    uba

--- index.js
+++ index.js
@@ -80,6 +80,10 @@ function verifyToken(req ,res ,next){
 
 if (process.env.NODE_ENV == 'production') {
     app.use(express.static("ubafront/build"));
+    const path= require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'ubafront','build','index.html'));
+    })
 }
 
 const port = process.env.PORT || 5000 ;
